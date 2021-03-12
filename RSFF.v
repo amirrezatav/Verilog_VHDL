cc161e900d8f91d75f60f417d0524773fe602bc1
Amirreza Tavakoli <amirrezatav2@gmail.com>
2021-03-12 20:25:30 +0330

    - 8-Bit Parallel to Serial Shift Register - SR Flip-Flop with  Preset and Clear

---
+++ RSFF.v
@@ -0,0 +1,21 @@
+/*
+SR Flip-Flop with  Preset and Clear
+*/
+module SRFF (
+    out, 
+    out_,
+    Set,
+    Reset,
+    CLR_,
+    PRE_,
+    CLK_ // Clock
+);
+    input Set , Reset , clear , CLK;
+    output out , out_ ;
+    // Level 1 
+    nand(w1,Set,CLK_);
+    nand(w2,Reset,CLK_);
+    // Level 2 
+    nand(out,w1,out_,PRE_);
+    nand(out_,w2,out,CLR_);
+endmodule
