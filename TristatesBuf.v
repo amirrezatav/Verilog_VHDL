16803794799432af911b712ad0651f578f841539
Amirreza Tavakoli <60503794+amirrezatav@users.noreply.github.com>
2021-03-13 15:10:44 +0330

    - TristatesBuf

---
+++ TristatesBuf.v
@@ -0,0 +1,22 @@
+module TristatesBuf (
+    out_,// Output (Active Low)
+    outEnable, //Data Input
+    dataInput//Output Enable
+    ); 
+    
+    input dataInput, outEnable; 
+    output out_; 
+    reg out_; //Use for left hand side (LHS) of signals assigned inside in always blocks
+    
+    always @(dataInput or outEnable) 
+    begin 
+        if (outEnable)
+        begin
+          not(dataInput_,dataInput);
+          out_ = dataInput_; 
+        end else begin
+          out_ = 1'bZ;
+        end 
+    end
+
+endmodule 
