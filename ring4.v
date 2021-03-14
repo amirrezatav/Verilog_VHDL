c929a9a886016a7efd877931aa3c8aa64444fb87
Amirreza Tavakoli <amirrezatav2@gmail.com>
2021-03-14 17:29:24 +0330

    - test

---
+++ ring4.v
@@ -0,0 +1,23 @@
+module ring4_test;
+reg clock , reset;
+wire [3:0] count;
+
+initial begin
+    clock = 1'b0;
+    forever begin
+        #50 clock = ~ clock;
+    end
+end
+
+initial begin
+    reset = 1'b1;
+    #200 reset = 1'b0;
+    #1600 $stop;
+end 
+
+initial begin
+    $monitor ($time , " clock = %d , \t reset = %d , \t count = %b  " , clock , reset , count);
+end 
+ring4 dut(clock , reset , count);
+
+endmodule
--- ring_counter.v
+++ ring_counter.v
@@ -1,18 +1,24 @@
 module ring_counter (
     clock,
     reset,
-    count
+    count,
+    mode, // 1 right counting , 0 left counting
+    load
 );
-    input clock , reset;
+    input clock , reset , mode;
+    input  [3:0] load;
     output count;
 
     reg [3:0] count;
 
-    always @(posedge clock or posedge reset) 
+    always @(  clock or posedge reset) 
     begin
         if(reset)
+        for (i = ; i < 4 ; i = i + 1 ) begin
+            count[i] = load[i];
+        end
             count = 4'b0001;
         else
-            count = {cout[2:0] , count[3]};
+            count = (mode) ? {cout[0], count[3:1]} : {cout[2:0] , count[3]};
     end
 endmodule
