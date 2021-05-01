31053f024a69dc2e67c1c1554e4a678e184a94b9
Amirreza Tavakoli <60503794+amirrezatav@users.noreply.github.com>
2021-05-01 05:55:31 +0430

    Create ArithTest.v

---
+++ ArithTest.v
@@ -0,0 +1,13 @@
+module ArithTest();
+reg [3:0] a ;
+reg [3:0] b;
+initial begin
+    a = 4'b1100;
+    b = 4'b0011;
+    $display("a+b = ", a+b);
+    $display("a-b = ", a-b);
+    $display("a/b = ", a/b);
+    $display("a*b = ", a*b);
+    $display("a%%b = ", a%b);
+end
+endmodule
