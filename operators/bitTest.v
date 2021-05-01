a475f4bdfb3587b6167b024d41259f9f795010c6
Amirreza Tavakoli <60503794+amirrezatav@users.noreply.github.com>
2021-05-01 05:54:08 +0430

    Create bitTest.v

---
+++ operators/bitTest.v
@@ -0,0 +1,15 @@
+module bitTest();
+reg [3:0] a ;
+reg [3:0] b;
+reg [3:0] c;
+initial begin
+    a = 4'b1100;
+    b = 4'b0011;
+    c = 4'b0101;
+    $display("~a = ", ~a);
+    $display("a & c = ", a & c);
+    $display("a | b = ", a | b);
+    $display("a ^ c = ", a ^ c);
+    $display("a ~^ c = ", a~^c);
+end
+endmodule
